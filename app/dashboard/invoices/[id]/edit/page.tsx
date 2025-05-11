1099182f800d7476254fad4557bc7585ed28aa5d
Anthony Elison <tonyelison37@gmail.com>
2025-05-11 09:59:14 -0600

    next tutorial ch. 13

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
+import { notFound } from "next/navigation";
 
 export default async function EditInvoicePage(props: {
   params: Promise<{ id: string }>;
@@ -11,6 +12,10 @@ export default async function EditInvoicePage(props: {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
+  if (!invoice) {
+    notFound();
+  }
 
   return (
     <main>
